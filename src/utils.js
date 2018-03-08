4707f1fbdea8a61c4186ab6d50eae65681be77a1
Benjamin Listwon <ben@benjaminlistwon.com>
2018-03-08 13:42:26 -0800

    Blob download fix. See: https://github.com/dropbox/dropbox-sdk-js/issues/172

--- src/utils.js
+++ src/utils.js
@@ -11,7 +11,8 @@ export function isWindowOrWorker() {
       typeof WorkerGlobalScope !== 'undefined' &&
       self instanceof WorkerGlobalScope
     ) || (
-      typeof module === 'undefined'
+      typeof module === 'undefined' || 
+      typeof window !== 'undefined'
     )
   );
 }
